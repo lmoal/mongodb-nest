cb84ad95ff085d5764f0df68551a703d06a3fec9
Laurent.Moal <laurent.moal@arkea.com>
2020-02-21 19:09:09 +0100

    delete nome_modules ...

--- dist/databases/database.interface.d.ts
+++
@@ -1,5 +0,0 @@
-import { Collection } from "../collections/collection.interface";
-export interface Database {
-    name: string;
-    collections: Collection[];
-}
--- dist/databases/databases.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from "./database.interface";
-export declare class DatabasesController {
-    private mongodbService;
-    getDatabases(): Database[];
-    createDatabase(body: string): void;
-    deleteDatabase(): void;
-}
